668f795e776042d6d2f070e1d06e8a07ccbf91b8
Rafael Tadeu <rtadeu@mmc.local>
2020-04-25 18:02:08 -0300

    service structure

---
+++ control/central.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+## nano /home/vagrant/.bashrc
+## alias dc='/docker/manager/central.sh'
+
+## Variaveis de controle
+ vpathdocker="/docker"
+ vsrv=$1
+
+ ## up service
+srvup(){
+ cd $vpathdocker/docker-compose-$vsrv
+ docker-compose up -d
+}
+## down service
+srvdown(){
+ cd $vpathdocker/docker-compose-$vsrv
+ docker-compose down
+}
+## start service
+srvstart(){
+ cd $vpathdocker/docker-compose-$vsrv
+ docker-compose start
+}
+## stop service
+srvstop(){
+ cd $vpathdocker/docker-compose-$svrv
+ docker-compose stop
+}
+
+case $2 in
+
+  up)
+    srvup
+    ;;
+
+  down)
+    srvdown
+    ;;
+
+  start)
+    srv start
+    ;;
+
+  stop)
+    srvstop
+    ;;
+
+  *)
+    echo "Escoplha uma das opções up | down | start | stop"
+    ;;
+esac
