49803cebf4c038101aaf26752a18837d5a0f4b81
Rafael Tadeu <rtadeu@mmc.local>
2020-05-08 18:16:29 -0300

    UpDate

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ## Executar antes de subir o servidor OCS Inventory
 sudo docker-compose build
+
+## Copiar o arquivo de configuracai do nginx
+docker exec srv-ws-nginx cp /tmp/center.conf /etc/nginx/conf.d/
+docker exec srv-ws-nginx service nginx reload
